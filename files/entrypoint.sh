e5b8147f110700b6d8f3ad9d60cdc677f3db9e84
Kurt Huwig <k.huwig@telekom.de>
2015-03-05 06:56:20 +0100

    Initial commit

---
+++ files/entrypoint.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+if [ ! -e /var/www/html/shopware.php ]; then
+    echo "Shopware not found, installing..."
+    rm -f /var/www/html/index.html \
+    && unzip -d /var/www/html /tmp/shopware.zip \
+    && chown -R www-data /var/www/html/logs/ \
+                         /var/www/html/config.php \
+                         /var/www/html/cache/general/ \
+                         /var/www/html/cache/html/ \
+                         /var/www/html/cache/templates/ \
+                         /var/www/html/cache/proxies/ \
+                         /var/www/html/cache/doctrine/* \
+                         /var/www/html/cache/mpdf/* \
+                         /var/www/html/files/* \
+                         /var/www/html/engine/Shopware/Plugins/Community/ \
+                         /var/www/html/engine/Shopware/Plugins/Default/ \
+                         /var/www/html/media/*
+    echo "done"
+fi
+if [ -f /etc/apache2/phpmyadmin.htpasswd ]
+then
+  HTPASSWD_OPTS=-Bbi
+else
+  HTPASSWD_OPTS=-cBbi
+fi
+
+if [ -n "$PHPMYADMIN_PW" ]; then
+    htpasswd -Bbc /etc/apache2/phpmyadmin.htpasswd phpmyadmin "${PHPMYADMIN_PW}"
+fi
+
+cat > /var/www/html/config.php << EOF 
+<?php
+return array(
+    'db' => array(
+        'username' => '${DB_USER:-shopware}',
+        'password' => '${DB_PASSWORD:-shopware}',
+        'dbname' => '${DB_DATABASE:-shopware}',
+        'host' => '${DB_HOST:-${DB_PORT_3306_TCP_ADDR}}',
+        'port' => '${DB_PORT:-${DB_PORT_3306_TCP_PORT:-3306}}'
+    )
+);
+EOF
+
+cat > /etc/phpmyadmin/config-db.php << EOF
+<?php
+\$dbuser='${DB_USER:-shopware}';
+\$dbpass='${DB_PASSWORD:-shopware}';
+\$basepath='';
+\$dbname='${DB_DATABASE:-shopware}';
+\$dbserver='${DB_HOST:-${DB_PORT_3306_TCP_ADDR}}';
+\$dbport='${DB_PORT:-${DB_PORT_3306_TCP_PORT:-3306}}';
+\$dbtype='mysql';
+EOF
+
+source /etc/apache2/envvars
+exec apache2 -D FOREGROUND
